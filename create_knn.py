8a595e75bbb82fd2dbe8a108eac325d2785313f3
belkacemm <belkacemmezhoud@comcast.net>
2023-06-11 17:33:26 -0500

    update p to be number_input

--- create_knn.py
+++ create_knn.py
@@ -4,7 +4,7 @@ import requests
 st.title("Calling knnapi fastapi")
 st.header("Get the input data from the user")
 neighbors = st.number_input("Enter a number of neighbors", min_value=1, max_value=50)
-p = st.selectbox("Select 1 for Manhattan, 2 for Euclidean and any number for Minkowski", [1, 2])
+p = st.number_input("Enter 1 for Manhattan, 2 for Euclidean and any other number for Minkowski", min_value=1, max_value=50)
 BASE_URL = "https://knnapi-1-w3784165.deta.app/knn/create?"
 url = BASE_URL + "neighbors=" + str(neighbors) + "&p=" + str(p)
 response = requests.get(url).json()
